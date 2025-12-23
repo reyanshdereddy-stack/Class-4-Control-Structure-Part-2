8064918ee1d001df3cfae5d02f94fc5b7672a5d1
reyanshdereddy-stack <reyanshdereddy@gmail.com>
2025-12-23 18:27:32 -0500

    v1

---
+++ Activity1.java
@@ -0,0 +1,16 @@
+class Activity1 {
+public static void main(String[] args) {
+    boolean sunny = true;
+    boolean hot = false;
+    if (sunny) {
+        System.out.println("Wear a cap and half-sleeves");
+        if (hot) {
+            System.out.println("Wear half-sleeves");
+        }
+    }
+    else{
+        System.out.println("Wear a raincoat for safety");
+    }
+
+}
+}
