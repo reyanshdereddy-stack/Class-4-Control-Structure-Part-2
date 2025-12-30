fa8edfecbee5f89eb5581706c943829e5c1f56c9
reyanshdereddy-stack <reyanshdereddy@gmail.com>
2025-12-30 10:16:32 -0500

    Afterclassprojectv2

--- AfterClassProject.java
+++ AfterClassProject.java
@@ -5,25 +5,30 @@ class AfterClassProject {
         Scanner s =  new Scanner (System.in);
         int num = s.nextInt();
         if (num<0 || num>100000) {
-            System.out.println("Invalid input.");
-            
+            System.out.println("Invalid input.");  
         }
-        if (num>=0 && num<5000) {
+        else if (num>=0 && num<5000) {
             System.out.println("This movie is bad.");
         }
-        if (num>=5000 && num<100000) {
+        else if (num>=5000 && num<25000) {
             System.out.println("This movie is good.");
-            if (num>=25000 && num<100000) {
-                System.out.println("This movie is great.");
-                if (num>=75000 && num<100000) {
-                    System.out.println("This movie is out of this world.");
-                }
-            }
         }
-        if (num>=45000 && num<70000){
+        else if (num>=25000 && num<45000) {
+            System.out.println("This movie is great.");
+        }
+        else if (num>=45000 && num<70000) {
             System.out.println("This movie is the best.");
+                }
+        else if (num>=70000 && num<75000){
+            System.out.println("Unspecified.");
+        }
+        else if (num>=75000){
+            System.out.println("This movie is out of this world.");
+            }
         }
         
         }
-    }
+        
+        
+    
 
